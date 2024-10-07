2431d43e146fa757bdfd4bbb35be620e6e7a9c70
elijahdunbar <eli.dunbar18@gmail.com>
2024-10-07 09:04:14 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,45 @@
+import time
+from turtle import Screen
+from scoreboard import Scoreboard
+from snake import Snake
+from food import Food
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Silly Snek")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_on = True
+while game_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    #Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.add_point()
+
+    #Detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.ycor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() < -280:
+        scoreboard.game_over()
+        game_on = False
+
+
+
+
+
+
+
+screen.exitonclick()
---
+++ scoreboard.py
@@ -0,0 +1,28 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("courier", 20, "normal")
+
+class Scoreboard(Turtle):
+
+  def __init__(self):
+    super().__init__()
+    self.penup()
+    self.ht()
+    self.goto(0, 270)
+    self.score = 0
+    self.refresh_score()
+
+
+  def refresh_score(self):
+    self.color("white")
+    self.clear()
+    self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
+  def add_point(self):
+    self.score += 1
+    self.refresh_score()
+
+  def game_over(self):
+    self.goto(0,0)
+    self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+
